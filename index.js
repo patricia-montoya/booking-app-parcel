07a7f460765a4fca2890eb7d0921740507a17602
Patricia Montoya <pmontoya@lean-tech.io>
2020-03-29 14:10:28 -0500

    Setup - Adding project dependencies and setting up Prettier and ESLint

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class HelloMessage extends React.Component {
-  render() {
-  return <div>Hello {this.props.name}</div>
-  }
-};
-
-var mountNode = document.getElementById("app");
-ReactDOM.render(<HelloMessage name="Pato" />, mountNode);
+ReactDOM.render(<h1>Hello!</h1>, document.getElementById('app'));
